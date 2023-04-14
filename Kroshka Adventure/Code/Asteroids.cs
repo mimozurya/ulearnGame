afd0cf6469a358d72429ccc55aa63744f5ff0755
mimo <egor_sidorenko007@mail.ru>
2023-04-14 17:50:16 +0500

    добавил персонажа и косые движения

--- Kroshka Adventure/Code/Asteroids.cs
+++ Kroshka Adventure/Code/Asteroids.cs
@@ -10,6 +10,7 @@ namespace Kroshka_Adventure
         public static Random rnd = new Random();
         static public SpriteBatch SpriteBatch { get; set; }
         static Wind[] winds;
+        static public Character Character { get; set; }
         static public int GetIntRnd (int min, int max)
         {
             return rnd.Next(min, max);
@@ -23,12 +24,14 @@ namespace Kroshka_Adventure
             winds = new Wind[50];
             for (int i = 0; i < winds.Length; i++)
                 winds[i] = new Wind(new Vector2(-rnd.Next(1, 10), 0));
+            Character = new Character(new Vector2(0, 0));
         }
 
         static public void Draw()
         {
             foreach (Wind wind in winds)
                 wind.Draw();
+            Character.Draw();
         }
 
         static public void Update()
@@ -77,4 +80,42 @@ namespace Kroshka_Adventure
             Asteroids.SpriteBatch.Draw(Texture2D, Pos, color);
         }
     }
+
+    class Character
+    {
+        public Vector2 Pos; // позиция
+        Color color = Color.White;
+        public int Speed { get; set; } = 3;
+
+        public static Texture2D Texture2D { get; set; }
+
+        public Character (Vector2 Pos)
+        {
+            this.Pos = Pos;
+        }
+
+        public void Up()
+        {
+            if (this.Pos.Y > 0) this.Pos.Y -= Speed;
+        }
+        public void Down()
+        {
+            if (this.Pos.Y < Asteroids.Height - Texture2D.Height) this.Pos.Y += Speed;
+        }
+        public void Left()
+        {
+            if (this.Pos.X > 0) this.Pos.X -= Speed;
+        }
+        public void Right()
+        {
+            if (this.Pos.X < Asteroids.Width - Texture2D.Width) this.Pos.X += Speed;
+        }
+
+        public void Draw()
+        {
+            Asteroids.SpriteBatch.Draw(Texture2D, Pos, color);
+        }
+    }
+
+
 }
